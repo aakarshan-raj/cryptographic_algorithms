4a07a828c99cc36966d3350f1621672460102c48
Aakarshan Raj <32811006+aakarshan-raj@users.noreply.github.com>
2022-02-07 16:23:22 +0530

    added condition for spaces

--- caesar's cipher.cpp
+++ caesar's cipher.cpp
@@ -6,8 +6,8 @@ string enc(string,int);
 string dec(string ciphertext,int key);
 int main(){
  
- cout<<enc("aakarshan",10)<<endl;
- cout<<dec(enc("aakarshan",10),10)<<endl;
+ cout<<enc("aakarshan dsf",10)<<endl;
+ cout<<dec(enc("aakarshan dsf",10),10)<<endl;
 
 }
 
@@ -18,9 +18,11 @@ string ciphertext;
 for(int x=0;x<plaintext.length();x++){
 
 cipher[x] = (((plaintext[x]-97)+key)%26)+65;
+if(cipher[x] == '>'){cipher[x] = ' ';}  //to deal with space
 ciphertext.append(1,cipher[x]);
 
 }
+
 return ciphertext;
 }
 
@@ -32,11 +34,13 @@ char plain[ciphertext.length()];
 for(int x = 0;x<ciphertext.length();x++){
 ciphertext[x] =  ciphertext[x]-65;    
 int p = ciphertext[x] - key;
+
 if(p<0){
     p = 26+p;
 }
 
 plain[x] = p+97;
+if(plain[x]=='P'){plain[x]=' ';}  //to deal with space
 plaintext.append(1,plain[x]);
 }
 return plaintext;
